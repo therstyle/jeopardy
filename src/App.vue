baed1480524b74a9751c697be0ff7c9c1018360b
Chris <therstyle@gmail.com>
2020-01-04 17:52:56 -0500

    Created intro component and structure cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    
+    <intro></intro>
   </div>
 </template>
 
 <script>
+import Intro from './components/intro.vue';
 
 export default {
   methods: {
@@ -18,6 +19,7 @@ export default {
     }
   },
   components: {
+    Intro
   },
   created() {
     this.loadData('sample.json');
@@ -26,24 +28,4 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/intro.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h1>Intro</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'intro'
+}
+</script>
