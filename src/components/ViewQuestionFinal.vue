531eedc6180fe7baf68ec49d68835b86e5fcb417
Chris <therstyle@gmail.com>
2020-01-31 08:50:45 -0500

    Added control panel to final jeopardy component

--- src/components/ViewQuestionFinal.vue
+++ src/components/ViewQuestionFinal.vue
@@ -1,26 +1,32 @@
 <template>
   <section class="view-question final-jeopardy">
-    <div v-if="!wagersSubmitted && !reveal">
-      {{ question.category }} <button v-on:click="viewQuestion">Continue</button>
-    </div>
+    <article>
+      <div v-if="!wagersSubmitted && !reveal">
+        {{ question.category }} <button v-on:click="viewQuestion">Continue</button>
+      </div>
 
-    <div v-if="wagersSubmitted && !reveal">
-      {{ question.question }} <button v-on:click="viewAnswer">Continue</button>
-    </div>
+      <div v-if="wagersSubmitted && !reveal">
+        {{ question.question }} <button v-on:click="viewAnswer">Continue</button>
+      </div>
 
-    <div v-if="wagersSubmitted && reveal">
-      {{ question.answer }} <app-button goTo="game-over">Show Results</app-button>
-    </div>
+      <div v-if="wagersSubmitted && reveal">
+        {{ question.answer }} <app-button goTo="game-over">Show Results</app-button>
+      </div>
+    </article>
+
+    <control-panel></control-panel>
   </section>
 </template>
 
 <script>
 import AppButton from './layout/AppButton.vue';
+import ControlPanel from './ControlPanel.vue';
 
 export default {
   name: 'final-jeopardy',
   components: {
-    AppButton
+    AppButton,
+    ControlPanel
   },
   data() {
     return {
