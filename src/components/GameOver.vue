60aacf45de82abc9ca0c58ff4540c992b503dd70
Chris <therstyle@gmail.com>
2020-02-01 20:14:18 -0500

    Added winner's names'

--- src/components/GameOver.vue
+++ src/components/GameOver.vue
@@ -1,12 +1,33 @@
 <template>
   <section class="game-over">
     <h1>Game Over</h1>
-    <p>Display the winners and stats here...</p>
+    
+    <div class="winners-circle">
+      <ul>
+        <li v-for="(leader, index) in leaders" :key="index">
+          {{ leader.name }}
+          Score: {{ leader.score }} <br>
+          Correct:{{ leader.correct }}<br>
+          Wrong: {{ leader.wrong }}<br>
+          Accuracy: {{ leader.accuracy }}
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'game-over'
+  name: 'game-over',
+  computed: {
+    players() {
+      const players = this.$store.getters.getPlayers;
+      return players;
+    },
+    leaders() {
+      const players = [...this.players];
+      return players.slice(0, 4);
+    }
+  }
 }
 </script>
