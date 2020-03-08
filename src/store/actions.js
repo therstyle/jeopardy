4646c4c2cb16cc5c088b5cd99a62ac7f895a0019
Chris <therstyle@gmail.com>
2020-03-08 17:39:16 -0400

    Breakdown store functions into seperate modules

---
+++ src/store/actions.js
@@ -0,0 +1,191 @@
+const setQuestions = (context, payload) => {
+  context.commit('setQuestions', payload);
+};
+
+const sortQuestions = (context, payload) => {
+  const data = [...payload];
+  const round1 = data.filter(question => question.round === 'Jeopardy!');
+  const round2 = data.filter(question => question.round === 'Double Jeopardy!');
+  const round3 = data.filter(question => question.round === 'Final Jeopardy!');
+  const questions = {
+    round1: round1,
+    round2: round2,
+    round3: round3
+  }
+
+  context.commit('setQuestions', questions);
+};
+
+const setCategories = function(context) {
+  const currentRound = this.state.round;
+  const categories = this.state.questions['round' + currentRound].map( data => {
+    return data.category;
+  });
+
+  const uniqueCategories = Array.from(new Set(categories));
+  context.commit('setCategories', uniqueCategories);
+};
+
+const setCurrentComponent = (context, payload) => {
+  context.commit('setCurrentComponent', payload);
+};
+
+const addPlayer = function(context, payload) {
+  const players = [...this.state.players, payload];
+  context.commit('setPlayers', players);
+};
+
+const removePlayer = function(context, payload) {
+  console.log('removing player');
+  const players = this.state.players;
+  const removedPlayers = players.filter(player => {
+    return player.name !== payload;
+  });
+
+  context.commit('setPlayers', removedPlayers);
+};
+
+const setScore = function(context, payload) {
+  const players = [...this.state.players];
+  const findPlayer = players.filter(player => player.id === payload.id);
+  const player = findPlayer[0];
+
+  console.log(player);
+
+  if (player) {
+    console.log(`updating score for player ${player.id}`);
+
+    player.score += parseInt(payload.score);
+    player.correct += payload.correct;
+    player.wrong += payload.wrong;
+
+    const total = player.wrong + player.correct;
+    let accuracy = (player.correct / total) * 100;
+    accuracy = Math.floor(accuracy);
+    player.accuracy = accuracy;
+
+    const answered = player.answered;
+    answered.push(this.state.currentQuestionId);
+  }
+
+  context.commit('setScore', payload);
+};
+
+const updateWager = (context, payload) => {
+  const player = context.getters.getCurrentPlayer;
+  player.wager = payload;
+
+  context.commit('updateWager', player);
+};
+
+const resetWager = (context, payload) => {
+  context.commit('resetWager', payload);
+};
+
+const setCurrentQuestionId = (context, payload) => {
+  context.commit('setCurrentQuestionId', payload);
+};
+
+const setCurrentPlayerId = (context, payload) => {
+  context.commit('setCurrentPlayerId', payload);
+};
+
+const turnComplete = function(context) {
+  const currentRound = this.state.round;
+  const questions = {...this.state.questions};
+  const currentId = this.state.currentQuestionId;
+  let i = 0;
+
+  questions['round' + currentRound].forEach(question => {
+    if (question.id === currentId) {
+      question.answered = true;
+    }
+  });
+
+  questions['round' + currentRound].forEach(question => {
+    if (question.answered) {
+      i++;
+    }
+  });
+
+  console.log(`questions answered = ${i}`);
+  console.log(questions['round' + currentRound].length);
+
+  if (i === questions['round' + currentRound].length) {
+    context.dispatch('setCurrentComponent', 'players');
+    context.dispatch('setRound');
+    console.log('start the next round');
+  }
+
+  context.commit('setQuestions', questions);
+};
+
+const setRound = function(context) {
+  const players = [...this.state.players];
+  let round = this.state.round;
+  round++;
+
+  if (round > 3) {
+    round = 1;
+  }
+
+  players.forEach(player => {
+    player.answered = [];
+  });
+  
+  console.log(`the round is... ${this.state.round}`);
+  context.commit('setRound', round);
+};
+
+const resetGame = function(context) {
+  const players = [...this.state.players];
+
+  players.forEach(player => {
+    player.score = 0;
+    player.correct = 0;
+    player.wrong = 0;
+    player.accuracy = 0;
+    player.answered = [];
+  });
+
+  const questions = {...this.state.questions};
+
+  questions.round1.forEach(question => question.answered = false);
+  questions.round2.forEach(question => question.answered = false);
+  questions.round3.forEach(question => question.answered = false);
+
+  context.commit('setPlayers', players);
+  context.dispatch('setRound');
+  context.dispatch('setCurrentComponent', 'intro');
+  context.dispatch('setCurrentQuestionId', 0);
+  context.dispatch('setQuestions', questions);
+};
+
+const setSound = function (context) {
+  const toggle = !this.state.sound;
+  context.commit('setSound', toggle);
+};
+
+const setPaused = function (context) {
+  const toggle = !this.state.paused;
+  context.commit('setPaused', toggle);
+};
+
+export {
+  setQuestions,
+  sortQuestions,
+  setCategories,
+  setCurrentComponent,
+  addPlayer,
+  removePlayer,
+  setScore,
+  updateWager,
+  resetWager,
+  setCurrentQuestionId,
+  setCurrentPlayerId,
+  turnComplete,
+  setRound,
+  resetGame,
+  setSound,
+  setPaused
+}
---
+++ src/store/mutations.js
@@ -0,0 +1,80 @@
+const setQuestions = (state, payload) => {
+  state.questions = payload;
+};
+
+const setCategories = (state, payload) => {
+  state.categories = payload;
+};
+
+const setCurrentComponent = (state, payload) => {
+  state.currentComponent = payload;
+};
+
+const setPlayers = (state, payload) => {
+  state.players = payload;
+};
+
+const setCurrentQuestionId = (state, payload) => {
+  state.currentQuestionId = payload;
+};
+
+const setCurrentPlayerId = (state, payload) => {
+  state.currentPlayerId = payload;
+};
+
+const setRound = (state, payload) => {
+  state.round = payload;
+};
+
+const updateWager = (state, payload) => {
+  const players = state.players;
+  const playerId = state.currentPlayerId;
+  players.forEach( player => {
+    if (player.id === playerId) {
+      player = payload;
+    }
+  });
+};
+
+const resetWager = (state, payload) => {
+  const players = state.players;
+  const playerId = state.currentPlayerId;
+  players.forEach(player => {
+    if (player.id === playerId) {
+      player.wager = payload;
+    }
+  });
+};
+
+const setScore = (state, payload) => {
+  const players = state.players;
+
+  players.forEach(player => {
+    if (player.id === payload.id) {
+      player = payload;
+    }
+  });
+};
+
+const setSound = (state, payload) => {
+  state.sound = payload;
+};
+
+const setPaused = (state, payload) => {
+  state.paused = payload;
+};
+
+export {
+  setQuestions,
+  setCategories,
+  setCurrentComponent,
+  setPlayers,
+  setCurrentQuestionId,
+  setCurrentPlayerId,
+  setRound,
+  updateWager,
+  resetWager,
+  setScore,
+  setSound,
+  setPaused
+}
